7ab1ced6757de123c575e9210b03b534cc8bc80e
Jason Thompson <jason@jasont.us>
2021-03-22 10:11:51 -0400

    modal function complete

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,13 @@ import Section from "./Section";
 import Sidebar from "./Sidebar";
 import Footer from "./Footer";
 
+const FIELDS = {
+  general: ["name", "email", "phone", "linkedin", "github"],
+  education: ["school", { dateRange: ["from", "to"] }, "degree"],
+  experience: ["company", "role", { dateRange: ["from", "to"] }],
+  skills: ["skill"],
+};
+
 function App() {
   const sections = [];
   for (const section in FIELDS) {
@@ -17,7 +24,14 @@ function App() {
         <div className="row">
           <div className="col-10">
             {sections.map((section, idx) => {
-              return <Section key={section} id={idx} title={section} fields={FIELDS[section]}/>;
+              return (
+                <Section
+                  key={section}
+                  id={idx}
+                  title={section}
+                  fields={FIELDS[section]}
+                />
+              );
             })}
           </div>
           <Sidebar />
@@ -29,10 +43,3 @@ function App() {
 }
 
 export default App;
-
-const FIELDS = {
-  general: ["name", "email", "phone", "linkedin", "github"],
-  education: ["school", { dateRange: ["from", "to"] }, "degree"],
-  experience: ["company", "role", { dateRange: ["from", "to"] }],
-  skills: ["skill"],
-};
